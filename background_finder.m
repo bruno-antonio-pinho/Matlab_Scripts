45ebbc9555c869e071dbe97ec8385da84890610c
Bruno Antônio de Pinho <bruno.antonio.pinho@gmail.com>
2016-10-24 16:11:26 -0200

    Incluido a função background finder feita apartir do arquivo ExtractBackground.m para criar uma iamgem de fundo apartir do video.

---
+++ background_finder.m
@@ -0,0 +1,39 @@
+function [ background ] = background_finder( video )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+bk_downsample = 300;
+vidHeight = size(video, 1);
+vidWidth = size(video, 2);
+nFrames = size(video, 3);
+
+%% First-iteration background frame
+background_frame = 0.00;
+for k = 1:bk_downsample:nFrames
+    background_frame = background_frame + double(video(:,:,k));
+end
+
+background_frame = bk_downsample*background_frame/(nFrames);
+
+%% Second-iteration background frame
+%This section re-calculates the background frame while attempting to
+%minimize the effect of moving objects in the calculation
+
+background_frame2 = 0.00;
+pixel_sample_density = 0.00;
+diff_frame = 0.00;
+bk_downsample = 50;
+
+for k = 1:bk_downsample:nFrames
+    diff_frame = abs(double(video(:,:,k)) - background_frame);
+    diff_frame = 1 - im2bw(uint8(diff_frame),.25);
+    pixel_sample_density = pixel_sample_density + diff_frame;
+    nonmoving = double(video(:,:,k)).*diff_frame;
+    background_frame2 = background_frame2 + nonmoving;
+end
+
+background = background_frame2./pixel_sample_density;
+
+
+end
+
